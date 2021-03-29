080fb517a4c49a0fa800e8891e351a2dd59eb9c7
Anthony <anthony.baldor@gmail.com>
2021-03-28 21:46:08 -0700

    edit bash for mp4

--- NXContainers/nxDetector/run_yolo.sh
+++ NXContainers/nxDetector/run_yolo.sh
@@ -1,2 +1,2 @@
-docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source 0 --weights yolov5x.pt --conf 0.4
+docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source DJI_0225_Trim.mp4 --weights yolov5x.pt --conf 0.4
 
