7169e993725329c9c93028df7d5492ca27b3f1b0
Anthony <anthony.baldor@gmail.com>
2021-03-28 21:38:50 -0700

    updated bash script

--- NXContainers/nxDetector/run_yolo.sh
+++ NXContainers/nxDetector/run_yolo.sh
@@ -1,2 +1,2 @@
-docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5 python3 detect.py --source 0 --weights yolov5x.pt --conf 0.4
+docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source 0 --weights yolov5x.pt --conf 0.4
 
