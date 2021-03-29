eafd476283a1c9e8aa12b969cb54e80e8c141955
Anthony <anthony.baldor@gmail.com>
2021-03-28 18:37:10 -0700

    detect_f.py

--- NXContainers/nxDetector/detect_f.py
+++ NXContainers/nxDetector/detect_f.py
@@ -55,7 +55,7 @@ def detect(save_img=False):
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    colors = [[numpy.random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
     if device.type != 'cpu':
