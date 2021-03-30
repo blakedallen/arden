a255b2735d4067c4b416eeadb137d393089e0c6d
Anthony <anthony.baldor@gmail.com>
2021-03-29 22:09:51 -0700

    some networking and .sh commands

--- NXContainers/nxDetector/run_yolo.sh
+++ NXContainers/nxDetector/run_yolo.sh
@@ -1,2 +1,3 @@
-docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source DJI_0225_Trim.mp4 --weights yolov5x.pt --conf 0.4
+# docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source DJI_0225_Trim.mp4 --weights yolov5x.pt --conf 0.4 --network arden
 
+docker run --privileged --runtime nvidia --rm -v /data:/data -e DISPLAY -v /tmp:/tmp -ti yolov5_final python3 detect_f.py --source victor_drone_video_trim.mp4 --weights yolov5s.pt --conf 0.4 --network arden
